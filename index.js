e4370a4524bcfef12a989d74b62c3fb49d9f7ab7
Bethany Zamba <Bethany.Zamba@gmail.com>
2018-06-10 13:46:21 -0700

    blep

---
+++ index.js
@@ -0,0 +1,11 @@
+// = means assign what's on the right to the left.
+let message = "Hello World!";
+message += "Put another string in there.";
+
+let number = 3 / 9;
+number -= 5;
+
+console.log(message + number);
+
+let drawbox red now
+console.log(draw the box)
